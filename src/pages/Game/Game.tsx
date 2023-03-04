1ee03752cc5a0b8a732b7d0e9b311ac39327a354
tapiotyynela <tapio.tyynela@gmail.com>
2023-03-04 19:22:28 +0200

    removed background colors

--- src/pages/Game/Game.tsx
+++ src/pages/Game/Game.tsx
@@ -1,19 +1,13 @@
-import { useEffect, useState } from "react"
-import { useLocation, useNavigate } from "react-router-dom"
-import { AuthContainer } from "../../common/AuthContainer"
-import { Button } from "../../common/Button"
-import Text from '../../common/Text'
-import TeamScore from "./components/TeamScore"
+import { useNavigate } from "react-router-dom";
 
 const Game: React.FC = () => {
-    const navigate = useNavigate()
-    // const [game, setGame] = useState(state.game)
+  const navigate = useNavigate();
 
-    return (
-        <AuthContainer style={{paddingTop: 100}}>
-                <Text text={`Starting team:`} size='l' font='Marvel, sans-serif'/>
-        </AuthContainer>
-    )
-}
+  return (
+    <div>
+      <p>Game</p>
+    </div>
+  );
+};
 
-export default Game
+export default Game;
