0436512abaa50d0e2ab299287f1370d1d517e6c9
tapiotyynela <tapio.tyynela@gmail.com>
2023-03-11 13:56:22 +0200

    get game by identifier

--- src/api/game.tsx
+++ src/api/game.tsx
@@ -1,16 +1,21 @@
-import { post, get } from "."
-import { INewGame } from "../types/game"
+import { post, get } from ".";
+import { INewGame } from "../types/game";
 
 export const createNewGame = async (data: INewGame) => {
-    const res = await post('/game/newGame', data)
-    return res.data
-}
+  const res = await post("/game/newGame", data);
+  return res.data;
+};
+
+export const getGameByIdentifier = async (gameIdentifier: string) => {
+  const res = await get(`/game/${gameIdentifier}`);
+  return res.data;
+};
 
 export const getAllPleyersGames = async (userId: number | undefined) => {
-    if (userId) {
-        const res = await get(`/game/list/${userId}`)
-        return res.data
-    } else {
-        console.log("NO USERID")
-    }
-}
+  if (userId) {
+    const res = await get(`/game/list/${userId}`);
+    return res.data;
+  } else {
+    console.log("NO USERID");
+  }
+};
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
-import { Button, Container, Input, Loading, Title } from "../../common";
+import { getGameByIdentifier } from "../../api/game";
+import { Button, Container, Input, Loading, Text, Title } from "../../common";
 import { Header } from "../../components/Header";
 import Navigation from "../../components/Navigation";
 import { useAuth } from "../../hooks/useAuth";
@@ -7,21 +8,46 @@ import { useAuth } from "../../hooks/useAuth";
 const Home = () => {
   const { user } = useAuth();
   const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [inputValue, setInputValue] = useState<string>("");
+  const [message, setMessage] = useState<string>("");
+  const [game, setGame] = useState(null);
 
-  const pressButton = () => {
+  const pressButton = async () => {
     setIsLoading(true);
-    setTimeout(() => setIsLoading(false), 2000);
+    const game = await getGameByIdentifier(inputValue);
+    if (game) {
+      setMessage("PELI LÖYTYI");
+    } else {
+      setMessage("EI PELIÄ");
+    }
+    setGame(game);
+    setIsLoading(false);
+    console.log(game);
   };
 
   return (
     <Container>
       <Header nickname={user?.nickName || ""} />
       <Title color="#E2BABA">Moniker</Title>
-      <Input placeholder="Search by game ID" width={200} />
+      <Input
+        onChange={(e) => setInputValue(e.target.value)}
+        value={inputValue}
+        placeholder="Search by game ID"
+        width={200}
+      />
       <Button color="#E2BABA" onClick={pressButton}>
         Search
       </Button>
       {isLoading && <Loading />}
+      {game ? (
+        <Text color="#E2BABA" size={30}>
+          {message}
+        </Text>
+      ) : (
+        <Text color="#E2BABA" size={30}>
+          {message}
+        </Text>
+      )}
       <Navigation />
     </Container>
   );
